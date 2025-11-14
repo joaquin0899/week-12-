9850c7dfcf3530938351f407944141261076fe6d
joaquin0899 <jrflores15@cps.edu>
2025-11-14 15:02:52 +0000

    lists

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -3,30 +3,88 @@
 
 # Topics Covered:
 # Creating lists, indexing, slicing, appending, popping, sorting, reversing.
-
+# lists are part of the collection family in python
 # Examples:
 
-my_list = ['apple', 'banana', 'cherry']
-print(my_list[0])         # apple
-print(my_list[1:])        # ['banana', 'cherry']
-
-my_list.append('grape')
+my_list = [1,2,3,4,5]
+print(my_list)
+print(len(my_list))
+print(type(my_list))
+print(my_list[0])
+print(my_list[1:4])
+print(my_list[1:])
+print(my_list[:-1])
+print(my_list[::-1])
+my_list.append(6)
+print(my_list)
+my_list.extend([7,8])
+my_list.extend([9,10,11])
+print(my_list)
+my_list.pop()
+print(my_list)
+my_list.pop()
+print(my_list)
+my_list.reverse()
+print(my_list)
+my_list.reverse()
+print(my_list)
+my_list.remove(9)
 print(my_list)
 
-my_list.pop(1)
+new_list = list(range(12,120))
+print(new_list)
+
+my_list.extend(new_list)
 print(my_list)
 
-numbers = [3, 1, 4, 2]
-numbers.sort()
-print(numbers)
+long_list = list(range(120, 500))
+my_list.extend(long_list)
+print(my_list)
+
+every_third_element = my_list[::3]
+print(every_third_element)
+
+print(my_list[::10])
+
+del my_list[::3]
+print(my_list)
 
 
-# Practice Problems:
+# .append - adds one to list
+# .extend - adds multiple to list
+# .pop - removes one from end of list
+# .remove - removes first occurence of value
+# .sort - sort in ascending order
+# .reverse -reverse order of list
 
-# Create a list with 5 of your favorite foods.
 
-# Print the second and last item.
+# my_list = ['apple', 'banana', 'cherry']
+# print(my_list[0])         # apple
+# print(my_list[1:])        # ['banana', 'cherry']
 
+# my_list.append('grape')
+# print(my_list)
+
+# my_list.pop(1)
+# print(my_list)
+
+# numbers = [3, 1, 4, 2]
+# numbers.sort()
+# print(numbers)
+
+
+# # Practice Problems:
+
+# # Create a list with 5 of your favorite foods.
+
+# fav_foods = ['pizza', 'tacos', 'chicken', 'fries', 'burgers']
+
+
+
+
+
+# # Print the second and last item.
+# print(fav_foods[1])
 # Add a new item using .append().
 
 # Remove the first item using .pop(0).
